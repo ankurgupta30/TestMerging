f859b411771f0770070db7c87599887d881285b5
ankurgupta30 <ankurgupta30@yahoo.co.in>
2018-07-11 17:24:36 +0530

    Update Test1.java

--- src/main/java/info/ankur/git/Test1.java
+++ src/main/java/info/ankur/git/Test1.java
@@ -4,13 +4,23 @@ public class Test1 {
 	public static void main(String[] str){
 
 		print("Tom1");
+		featureForFuture1("This is not meant for production");
 		print("Dick");
 		print("Harry1");
 		print("Sherry");
 		print("Dick");
+		featureForFuture("This is not meant for production");
 	}
-	
+	private static void featureForFuture(String str){
+			System.out.println("Hello " + str);
+		}
+	}	
 	private static void print(String str){
 		System.out.println("Hello " + str);
+	}
+
+	private static void featureForFuture1(String str){
+			System.out.println("Hello " + str);
+		}
 	}
 }
