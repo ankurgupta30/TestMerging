fd92fa8eaf30e004e5e329af747579eaa80af573
ankurgupta30 <ankurgupta30@yahoo.co.in>
2018-07-11 16:46:13 +0530

    Initial Commit

---
+++ src/main/java/info/ankur/git/Test1.java
@@ -0,0 +1,13 @@
+package info.ankur.git;
+
+public class Test1 {
+	public static void main(String[] str){
+		print("Tom");
+		print("Dick");
+		print("Harry");
+	}
+	
+	private static void print(String str){
+		System.out.println("Hello " + str);
+	}
+}
